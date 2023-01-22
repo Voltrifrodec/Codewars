e194515254886dd843e732fdc0e8486282f2e4d9
V0ltaroo <nendorans@gmail.com>
2023-01-22 22:16:21 +0100

    2nd try on Happy Numbers (6kyu)

--- 6kyu/028_happy-numbers.js
+++ 6kyu/028_happy-numbers.js
@@ -1,4 +1,4 @@
-// Happy Numbers
+// Happy Numbers - 1st try (2023-01-22)
 function isHappy(n) {
     // Good Luck
     if(n == 1)
@@ -17,3 +17,25 @@ function isHappy(n) {
     
     return temp == 1;
 }
+
+
+// Happy Numbers - 2nd try (2023-01-22)
+function isHappy(n) {
+  // Good Luck
+  if(n == 1)
+    return true;
+  
+  let temp = 0;
+  let iterator = 1;
+  n = n.toString();
+  while(temp != 1 && iterator != 200) {
+    temp = (Array.from(n).map((a) => a ** 2).reduce((a,b) => a + b));
+    n = temp.toString();
+    iterator++;
+  }
+  
+  return temp == 1;
+}
+
+
+//? Note: 200 is just a imaginary value of iterations cycle should do... if it doesn't find in first 200 cycles, then it jumps outs with either true of false (depends on luck)
